16683d5bbadbdde758771d9d941dcb29162e29b0
alurui4987 <alurui4987@ieselcaminas.org>
2023-09-21 09:17:14 +0200

    Practica 3

---
+++ Practica_3/Ejercicio_3 Practica 3.ps1
@@ -0,0 +1,13 @@
+﻿<# Ejercicio 3. Crea un script en el que se pida dos números enteros al usuario. El script debe indicar 
+si el primer número es mayo, menor o igual que el otro.#>
+
+[int]$numero1 = Read-Host "Escribe un número: "
+[int]$numero2 = Read-Host "Escribe un número: "
+
+If ($numero1 -gt $numero2){
+        Write-Host "Tu número es mayor"
+}ElseIf($numero1 -eq $numero2){
+        Write-Host "Tus números son iguales"
+}Else{
+        Write-Host "Tu número es menor"
+}
---
+++ Practica_3/Ejercicio_6 Practica 3.ps1
@@ -0,0 +1,11 @@
+﻿<# Ejercicio 6. Crea unque diga si lo que se pasa por teclado es un directorio. En ese caso sacará una lista con los 
+archivos que contiene y subdirectorios. Debes utilizar el parámetro Recurse. #>
+
+$directorio = Read-Host "Ingresa una ruta (directorio)"
+
+
+if (Test-Path -Path $directorio -PathType Container) {
+    Write-Host "La ruta $directorio es un directorio."
+} else {
+    Write-Host "La ruta $directorio no es un directorio o no existe."
+}
