33faf07d238bab779396ba4b07dd27d08bdb943d
alurui4987 <alurui4987@ieselcaminas.org>
2023-09-21 09:50:46 +0200

    Más ejercicios

---
+++ Practica_3/Ejercicio_7 Practica 3.ps1
@@ -0,0 +1,15 @@
+﻿<# Ejercicio 7. Escribir un programa que alamacene la cadena de caracteres 
+contraseña en una variable, pregunte al usuario por la contraseña e 
+imprima por pantalla si la contraseña introducida por el 
+usuario coincide con la guardada en la variable sin tener en 
+cuenta mayúsculas y minúsculas.#>
+
+$contraseña = "asix"
+
+[string]$pregcontra = Read-Host "Cual es tu contraseña? "
+
+if ($pregcontra -ieq $contraseña){
+    Write-Host = "Tu contraseña coincide"
+}Else{
+    Write-Host = "Tu contraseña no coincide"
+}
