7f440f056ab52ab86d85e839f744e0f1abd0a14b
alurui4987 <alurui4987@ieselcaminas.org>
2023-09-25 09:42:16 +0200

    Más ejercicios de la Practica 3

--- Practica_3/copia/Ejercicio_10 Practica 3.ps1
+++
@@ -1,14 +0,0 @@
-﻿<#Ejercicio 10. En una determinada empresa, sus empleados son evaluados al final de cada año. Los puntos que pueden obtener en la evaluación  comienzan en 0.0 y pueden ir aumentando, traduciéndose en mejores  beneficios. Los puntos que pueden conseguir los empleados  pueden ser 0.0, 0.4, 0.6 o más, pero no valores intermedios entre las  cifras mencionadas. A continuación se muestra una tabla con los niveles correspondientes a cada puntuación. La cantidad de dinero conseguida  en cada nivel es de 2.400€ multiplicada por la puntuación del nivel.  Nivel Puntuación Inaceptable 0.0 Aceptable 0.4 Meritorio 0.6 o más#>[double]$puntuacion = Read-Host "Escribe tu puntuación"
-
-if ($puntuacion -eq 0.0) {
-    $dinero = $puntuacion * 2400
-    Write-Host "Tu puntuación es Horrible, te corresponden $dinero €"
-} elseif ($puntuacion -eq 0.4) {
-    $dinero = $puntuacion * 2400
-    Write-Host "Tu puntuación es Aceptable, te corresponden $dinero €"
-} elseif ($puntuacion -ge 0.6) {
-    $dinero = $puntuacion * 2400
-    Write-Host "Tu puntuación es Perfecta, te corresponden $dinero €"
-} else {
-    Write-Host "Tu puntuación debe ser 0.0, 0.4 o estar por encima de 0.6"
-}
--- Practica_3/copia/Ejercicio_5 Practica 3.ps1
+++
@@ -1,9 +0,0 @@
-﻿<# Ejercicio 5. Crea un script en el que pidas un fichero o carpeta por teclado y te diga si existe o no.#>
-
-$archivo = Read-Host "Ingresa el nombre de un archivo o carpeta"
-
-if (Test-Path $archivo) {
-    Write-Host "El archivo o carpeta $archivo existe."
-} else {
-    Write-Host "El archivo o carpeta $archivo no existe."
-}
