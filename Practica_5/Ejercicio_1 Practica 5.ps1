3ba88cc29b880fe978104b67b95cc8e7db0e731b
alurui4987 <alurui4987@ieselcaminas.org>
2023-10-02 09:24:28 +0200

    Ejercicios practica 5

---
+++ Practica_5/Ejercicio_1 Practica 5.ps1
@@ -0,0 +1,64 @@
+﻿<# Ejercicio 1 Crea una calculadora muy sencilla, en la que 
+se preguntará al usuario dos números y que operación desea realizar.
+
+Ejemplo:
+
+****** CALCULADORA ********
+
+    Sumar
+    Restar
+    Multiplicar
+    Dividir
+    Salir
+
+¿Qué desea hacer?Elige una opción:
+
+Debes crear las funciones Sumar, Restar, Multiplicar y Dividir. 
+Cada una de estas funciones tendrá dos parámetros.#>
+
+Function Sumar ($num1, $num2){
+    $sumar = $num1 + $num2
+    Write-Host "El resultado es $sumar"
+}
+
+Function Restar ($num1, $num2){
+    $restar = $num1 - $num2
+    Write-Host "El resultado es $restar"
+}
+
+Function Multiplicar ($num1, $num2){
+    $multiplicar = $num1 * $num2
+    Write-Host "El resultado es $multiplicar"
+}
+
+Function Division ($num1, $num2){
+    $division = $num1 / $num2
+    Write-Host "El resultado es $division"
+}
+
+Function Salir ($num1, $num2){
+    Write-Host "Hasta luego"
+}
+
+do{
+
+ $operacion = Read-host "`n 1.Sumar. `n 2. Resta. `n 3.Multiplicar. `n 4.Dividir. `n 5.Salir `n ¿Qué desea hacer?Elige una opción: "
+
+    if($operacion -ne 5){[double]$num1 = Read-Host "Escribe un número "
+    [double]$num2 = Read-Host "Escribe otro número "
+    }
+
+switch ($operacion){
+        1{
+            Sumar $num1 $num2}
+        2{
+            Resta $num1 $num2}
+        3{
+            Multiplicar $num1 $num2}
+        4{
+            Division $num1 $num2}
+        5{
+            Salir
+            }
+}
+}while($operacion -ne 5)
---
+++ Practica_5/Ejercicio_2 Practica 5.ps1
@@ -0,0 +1,9 @@
+﻿<#Ejercicio 2 Haciendo uso de la estructura repetitiva foreach, 
+debes leer todos los datos del archivo usuarios.csv e imprimir el nombre, 
+apellidos y grupo del usuario#>
+
+$usuarios = Import-Csv C:\Users\Administrador\Desktop\usuarios.csv -Delimiter ","
+
+foreach ($usu in $usuarios){
+    write-host "Usuario: $($usu.nombre) $($usu.apellidos) $($usu.grupo) "
+}
