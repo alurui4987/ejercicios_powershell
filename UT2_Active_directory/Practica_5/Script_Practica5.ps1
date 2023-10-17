23aeb8e64d8411da0bafe57869291e4529990b4f
alurui4987 <alurui4987@ieselcaminas.org>
2023-10-17 13:42:24 +0200

    Script Practica5_UT2

---
+++ UT2_Active_directory/Practica_5/Script_Practica5.ps1
@@ -0,0 +1,15 @@
+﻿New-Item -Path C:\Empresa -ItemType Directory
+
+$departamentos = Import-Csv C:\Users\Administrador\Documents\departamentos.csv -Delimiter ";"
+
+foreach ($dep in $departamentos){
+    
+    $depaNombre = $dep.departamento
+
+    New-Item -Path C:\Empresa\$depaNombre -ItemType Directory
+}
+
+foreach ($dep in $departamentos){
+    New-SmbShare -Path C:\Empresa\"$($dep.departamento)" -Name "$($dep.departamento)" -ChangeAccess "$($dep.departamento)" -FullAccess "Administradores" -ReadAccess "Usuarios del dominio"
+}
+ 
