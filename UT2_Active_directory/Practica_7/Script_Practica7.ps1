3f50e5c201636e891eccd9b54fd22b903afc6390
alurui4987 <alurui4987@ieselcaminas.org>
2023-10-30 08:59:14 +0100

    Script Practica 7

---
+++ UT2_Active_directory/Practica_7/Script_Practica7.ps1
@@ -0,0 +1,7 @@
+﻿ $empleados = Import-Csv C:\Users\Administrador\Documents\empleados.csv -Delimiter ";"
+
+ foreach($em in $empleados){
+    if($em.departamento -ieq "PERSONAL"){
+        Set-ADUser -identity "$($em.nombre).$($em.apellido)" -ProfilePath "\\Empresa-dc1\Empresa_users$\$($em.nombre).$($em.apellido)"
+    }
+ }
